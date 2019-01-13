ec7358908ebf551b8b876a1bd076b850dad5cea3
Oliver Renwick <oliver.renwick@gmail.com>
2019-01-13 10:43:33 -0500

    starting final edits, hopefully

--- liri.js
+++ liri.js
@@ -4,7 +4,13 @@ var keys = require("./keys.js");
 
 var axios = require("axios");
 
-artist = "Backstreet Boys"
+const lolcatjs = require('lolcatjs');
+
+var Spotify = require('node-spotify-api');
+
+var command = process.argv[2];
+
+fs = require('fs');
 
 //   * You should then be able to access your keys information like so
 
@@ -14,36 +20,93 @@ artist = "Backstreet Boys"
 
 // Make a switch function for Step 9 in the instructions
 
-var command = process.argv[2];
-
+// Switch function to decide between commands
 switch (command) {
 
   // Here's the concert code
   case "concert-this":
-    console.log("concert this!")
+    // Store all of the arguments in an array
+    var nodeArgs = process.argv;
+
+    // Create an empty variable for holding the movie name
+    var bandName = "";
+
+    // Loop through all the words in the node argument
+    // And do a little for-loop magic to handle the inclusion of "+"s
+    for (var i = 3; i < nodeArgs.length; i++) {
+
+      if (i > 3 && i < nodeArgs.length) {
+        bandName = bandName + "+" + nodeArgs[i];
+      }
+      else {
+        bandName += nodeArgs[i];
+      }
+    }
+
+    console.log("Here's some upcoming concerts!")
+
     axios
-      .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+      .get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp")
       .then(function (response) {
-        // What's not working is here. I don't know how to make the var key in function structure to work.
-        for (var key in response) {
-          // Spit out all venues in response
-          console.log(venue);
+        for (let i = 0; i < response.data.length; i++) {
+          const element = response.data[i];
+          console.log('\n' + response.data[i].venue.name);
+          console.log(response.data[i].venue.city);
+          console.log(response.data[i].datetime);
         }
-      })
-    // code block
+      }
+      )
     break;
 
 
   // Here's the Spotify code
   case "spotify-this-song":
+
     console.log("spotify this song!")
     // code block
+
+  // Store all of the arguments in an array
+  var nodeArgs = process.argv;
+
+  // Create an empty variable for holding the song name
+  var songName = "";
+
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "+"s
+  for (var i = 3; i < nodeArgs.length; i++) {
+
+    if (i > 3 && i < nodeArgs.length) {
+      songName = songName + "+" + nodeArgs[i];
+    }
+    else {
+      songName += nodeArgs[i];
+    }
+  }
+
+  if (songName === ""){
+    songName = "(I saw) The Sign"
+  }
+
+console.log(songName)
+
+  var spotify = new Spotify(keys.spotify);
+   
+  spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+  console.log("Artist's Name: " + data.tracks.items[0].artists[0].name); 
+  console.log("Name of Song: " + data.tracks.items[0].name); 
+  console.log("Preview: " + data.tracks.items[0].preview_url); 
+  console.log("Name of Album: " + data.tracks.items[0].album.name); 
+  });
     break;
 
 
   // Here's the OMDB code
   case "movie-this":
-    console.log("movie this!")
+
     // code block
     // Store all of the arguments in an array
     var nodeArgs = process.argv;
@@ -60,30 +123,23 @@ switch (command) {
       }
       else {
         movieName += nodeArgs[i];
-
       }
     }
     // Then run a request with axios to the OMDB API with the movie specified, with an if loop 
     // to provide for the user potentially not inputting information
-    if (movieName !== ""){
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-    // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
-
-    axios.get(queryUrl).then(
-      function (response) {
-        console.log(response.data.Ratings[1].Source)
-        console.log("Your movie: " + response.data.Title + ", " + response.data.Year + ", imdb Rating: " + response.data.imdbRating  + ", Rotten Tomatoes rating: " + response.data.Ratings[1].Value  + ", " + response.data.Country   + ", " + response.data.Language  + ", " + response.data.Plot   + " " + response.data.Actors  );
-      }
-    );
-  }
-  else {
-    console.log ("If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>")
-    console.log("It's on Netflix!")
-  }
+    if (movieName !== "") {
+      var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+      axios.get(queryUrl).then(
+        function (response) {
+          console.log("Your movie: " + response.data.Title + ", " + response.data.Year + ", imdb Rating: " + response.data.imdbRating + ", Rotten Tomatoes rating: " + response.data.Ratings[1].Value + ", " + response.data.Country + ", " + response.data.Language + ", " + response.data.Plot + " " + response.data.Actors);
+        }
+      );
+    }
+    else {
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>")
+      console.log("It's on Netflix!")
+    }
     break;
-
 
   // Here's the rando code
   case "do-what-it-says":
---
+++ liri.sync-conflict-20190110-125604-KVE7HCD.js
@@ -0,0 +1,15 @@
+js
+require("dotenv").config();
+
+js
+  var keys = require("./keys.js");
+
+
+//   * You should then be able to access your keys information like so
+
+//   ```js
+//   var spotify = new Spotify(keys.spotify);
+//   ```
+
+
+// Make a switch function for Step 9 in the instructions 
