95f22d4ec3e71d331c9c7ef8411f061f604b50b0
Oliver Renwick <oliver.renwick@gmail.com>
2019-01-13 22:44:42 -0500

    fully working bruh

--- liri.js
+++ liri.js
@@ -10,111 +10,87 @@ var Spotify = require('node-spotify-api');
 
 var command = process.argv[2];
 
-var file = require('file-system');
-
 var fs = require('fs');
 
-var node = require('node');
 
+// Spotify function
+function spotification(){
+  // code block
 
-//   * You should then be able to access your keys information like so
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
 
-//   ```js
-//   var spotify = new Spotify(keys.spotify);
-//   ```
+// Create an empty variable for holding the song name
+var songName = "";
 
-// Make a switch function for Step 9 in the instructions
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 3; i < nodeArgs.length; i++) {
 
-// Switch function to decide between commands
-switch (command) {
+  if (i > 3 && i < nodeArgs.length) {
+    songName = songName + "+" + nodeArgs[i];
+  }
+  else {
+    songName += nodeArgs[i];
+  }
+}
 
-  // Here's the concert code
-  case "concert-this":
-    // Store all of the arguments in an array
-    var nodeArgs = process.argv;
+if (songName === ""){
+  songName = "(I saw) The Sign"
+}
 
-    // Create an empty variable for holding the movie name
-    var bandName = "";
+console.log(songName)
 
-    // Loop through all the words in the node argument
-    // And do a little for-loop magic to handle the inclusion of "+"s
-    for (var i = 3; i < nodeArgs.length; i++) {
+var spotify = new Spotify(keys.spotify);
 
-      if (i > 3 && i < nodeArgs.length) {
-        bandName = bandName + "+" + nodeArgs[i];
-      }
-      else {
-        bandName += nodeArgs[i];
-      }
-    }
+spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
 
-    console.log("Here's some upcoming concerts!")
+console.log("Artist's Name: " + data.tracks.items[0].artists[0].name); 
+console.log("Name of Song: " + data.tracks.items[0].name); 
+console.log("Preview: " + data.tracks.items[0].preview_url); 
+console.log("Name of Album: " + data.tracks.items[0].album.name); 
+});
 
-    axios
-      .get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp")
-      .then(function (response) {
-        for (let i = 0; i < response.data.length; i++) {
-          const element = response.data[i];
-          console.log('\n' + response.data[i].venue.name);
-          console.log(response.data[i].venue.city);
-          console.log(response.data[i].datetime);
-        }
-      }
-      )
-    break;
+}
 
-
-  // Here's the Spotify code
-  case "spotify-this-song":
-
-    console.log("spotify this song!")
-    // code block
-
+function concertThis(){
   // Store all of the arguments in an array
   var nodeArgs = process.argv;
 
-  // Create an empty variable for holding the song name
-  var songName = "";
+  // Create an empty variable for holding the movie name
+  var bandName = "";
 
   // Loop through all the words in the node argument
   // And do a little for-loop magic to handle the inclusion of "+"s
   for (var i = 3; i < nodeArgs.length; i++) {
 
     if (i > 3 && i < nodeArgs.length) {
-      songName = songName + "+" + nodeArgs[i];
+      bandName = bandName + "+" + nodeArgs[i];
     }
     else {
-      songName += nodeArgs[i];
+      bandName += nodeArgs[i];
     }
   }
 
-  if (songName === ""){
-    songName = "(I saw) The Sign"
+  console.log("Here's some upcoming concerts!")
+
+  axios
+    .get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp")
+    .then(function (response) {
+      for (let i = 0; i < response.data.length; i++) {
+        const element = response.data[i];
+        console.log('\n' + response.data[i].venue.name);
+        console.log(response.data[i].venue.city);
+        console.log(response.data[i].datetime);
+      }
+    }
+    )
   }
 
-console.log(songName)
-
-  var spotify = new Spotify(keys.spotify);
-   
-
-  function spotification(){
-  spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-
-  console.log("Artist's Name: " + data.tracks.items[0].artists[0].name); 
-  console.log("Name of Song: " + data.tracks.items[0].name); 
-  console.log("Preview: " + data.tracks.items[0].preview_url); 
-  console.log("Name of Album: " + data.tracks.items[0].album.name); 
-  });
-
-}
-spotification();
-    break;
-
-  // Here's the OMDB code
-  case "movie-this":
+  function movieThis(){
     // code block
     // Store all of the arguments in an array
     var nodeArgs = process.argv;
@@ -145,23 +121,51 @@ spotification();
       console.log("If you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>")
       console.log("It's on Netflix!")
     }
-    break;
+  }
 
-  // Here's the rando code
-  case "do-what-it-says":
-    console.log("do what it says!")
+  // The do-what-it-says function
+  function random(){
     // code block
     var content;
     fs.readFile('./random.txt', function read(err, data) {
         if (err) {
             throw err;
         }
-        content = data;
-        
+        content = data.toString();
+        spotification(content);
     });
+  }
 
+
+function liri(){
+
+// Switch function to decide between commands
+switch (command) {
+  // Here's the concert code
+  case "concert-this":
+    concertThis();
+    break;
+
+  // Here's the Spotify code
+  case "spotify-this-song":
+    spotification();
+    break;
+
+  // Here's the OMDB code
+  case "movie-this":
+    movieThis();
+  break;
+
+  // Here's the rando code
+  case "do-what-it-says":
+    random()
     break;
   default:
     // code block
     console.log("That's not an option. Please choose a valid option.")
 }
+
+// End liri function
+}
+
+liri();
